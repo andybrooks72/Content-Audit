2a5674c682dc2ada246998ce5ca454087eccda79
Andy Brooks <andrew.brooks@pepper.money>
2025-03-18 11:21:44 +0000

    Removed vendor from .gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2ccc6a659e874cce7c83d81de9d7dab78dcca418',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2ccc6a659e874cce7c83d81de9d7dab78dcca418',
+            'dev_requirement' => false,
+        ),
+        'doctrine/inflector' => array(
+            'pretty_version' => '2.0.10',
+            'version' => '2.0.10.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../doctrine/inflector',
+            'aliases' => array(),
+            'reference' => '5817d0659c5b50c9b950feb9af7b9668e2c436bc',
+            'dev_requirement' => false,
+        ),
+        'stoutlogic/acf-builder' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stoutlogic/acf-builder',
+            'aliases' => array(),
+            'reference' => 'e63ab87233ea3675cd519f1dd6b6d4230b3cef97',
+            'dev_requirement' => false,
+        ),
+    ),
+);
